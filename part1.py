efddd33c95710f64f0d3d217010a0df8f9801170
Max Gilespie <max.gillespie10@gmail.com>
2019-04-04 08:45:54 -0500

    data structures

--- part1.py
+++ part1.py
@@ -7,9 +7,20 @@ import csv
 f = open("Lab8_feature_data.csv")
 
 data = [row for row in csv.reader(f)]
+x = np.delete(data, np.s_[72:74], axis=1)
+y = np.delete(data, np.s_[0:73], axis=1)
 
-for row in data:
-    print("--------NEW ROW--------")
-    for cell in row:
-        scrubbed_cell = cell.replace('(','').replace(')','')
-        print( scrubbed_cell )
+for i in range(0, len(data)):
+    for j in range(0, len(data[i]) - 2):
+        x[i][j] = data[i][j].replace('(','').replace(')','')
+
+
+# PROOF Y IS WORKING
+# for key in y:
+#     print(key[0])
+
+# PROOF X IS WORKING
+# for row in x:
+#     print("---------NEW ROW--------")
+#     for cell in row:
+#         print(cell)
