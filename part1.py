0c564bb6511880e6c5dc839126c184634f3f9ad5
Max Gilespie <max.gillespie10@gmail.com>
2019-04-04 09:24:11 -0500

    first ml

--- part1.py
+++ part1.py
@@ -1,19 +1,37 @@
 #ignore complex numbers going into the matrix
 
 import sklearn
+from sklearn import preprocessing
+from sklearn.ensemble import RandomForestRegressor
 import numpy as np
 import csv
 
 f = open("Lab8_feature_data.csv")
+f.readline()
 
 data = [row for row in csv.reader(f)]
-x = np.delete(data, np.s_[72:74], axis=1)
+for i in range(0, len(data)):
+    for j in range(0, len(data[i])):
+        data[i][j] = data[i][j].replace('(','').replace(')','')
+        data[i][j] = np.complex(data[i][j]).real
+
+data = preprocessing.scale(data)
+
+x = np.delete(data, np.s_[71:73], axis=1)
 y = np.delete(data, np.s_[0:73], axis=1)
 
-for i in range(0, len(data)):
-    for j in range(0, len(data[i]) - 2):
-        x[i][j] = data[i][j].replace('(','').replace(')','')
+x_test = x[23552:29539]
+x_train = x[0:23552]
+y_test = y[23552:29539]
+y_train = y[0:23552]
 
+print(y_test)
+
+# linreg = RandomForestRegressor(oob_score=True)
+#
+# linreg.fit(x_train, y_train)
+# y_pred = linreg.predict(x_test)
+# print(y_pred)
 
 # PROOF Y IS WORKING
 # for key in y:
